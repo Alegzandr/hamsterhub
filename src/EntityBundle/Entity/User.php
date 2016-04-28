2d24d99ecc6a73436d0a87602395f7adb5998612
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-04-28 13:50:30 +0200

    Add FOSUserBundle

---
+++ src/EntityBundle/Entity/User.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace EntityBundle\Entity;
+
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * User
+ */
+class User extends BaseUser
+{
+    /**
+     * @var int
+     */
+    protected $id;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
+
