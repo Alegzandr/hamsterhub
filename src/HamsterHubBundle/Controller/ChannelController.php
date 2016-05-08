f37e4a7fae20d4a2712a4d34c4c6e3a1d652103a
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-05-08 23:30:41 +0200

    Add channels and dashboards

--- src/HamsterHubBundle/Controller/ChannelController.php
+++ src/HamsterHubBundle/Controller/ChannelController.php
@@ -8,6 +8,14 @@ class ChannelController extends Controller
 {
     public function indexAction($username)
     {
-        return $this->render('HamsterHubBundle:Channel:index.html.twig', array('username' => $username));
+        $user = $this->getDoctrine()
+            ->getRepository('EntityBundle:User')
+            ->findOneByusername($username);
+
+        $url = $this->getDoctrine()
+            ->getRepository('EntityBundle:Video')
+            ->findBy(array('authorId'=>$user->getId()), array('id' => 'desc'));
+
+        return $this->render('HamsterHubBundle:Channel:index.html.twig', array('user'=>$user, 'url'=>$url));
     }
 }
--- src/HamsterHubBundle/Controller/DashboardController.php
+++ src/HamsterHubBundle/Controller/DashboardController.php
@@ -9,7 +9,15 @@ class DashboardController extends Controller
     public function indexAction($username)
     {
         if ($username === $this->getUser()->getUsernameCanonical()) {
-            return $this->render('HamsterHubBundle:Dashboard:index.html.twig', array('username' => $username));
+            $user = $this->getDoctrine()
+                ->getRepository('EntityBundle:User')
+                ->findOneByusername($username);
+
+            $url = $this->getDoctrine()
+                ->getRepository('EntityBundle:Video')
+                ->findBy(array('authorId'=>$user->getId()), array('id' => 'desc'));
+            
+            return $this->render('HamsterHubBundle:Dashboard:index.html.twig', array('user'=>$user, 'url'=>$url));
         } else {
             header('Location: /channel/' . $username);
             exit;
