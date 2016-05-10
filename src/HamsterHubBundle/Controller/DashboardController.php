20febf8c81abc9bb8d39ecefe1e1fca978254572
Alexandre Farrenq <alexandrefarrenq@gmail.com>
2016-05-10 14:48:40 +0200

    Add remove video functionnality

--- src/HamsterHubBundle/Controller/DashboardController.php
+++ src/HamsterHubBundle/Controller/DashboardController.php
@@ -6,21 +6,48 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DashboardController extends Controller
 {
-    public function indexAction($username)
+    public function indexAction()
     {
-        if ($username === $this->getUser()->getUsernameCanonical()) {
+        if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
             $user = $this->getDoctrine()
                 ->getRepository('EntityBundle:User')
-                ->findOneByusername($username);
+                ->findOneById($this->container->get('security.context')->getToken()->getUser()->getId());
 
             $url = $this->getDoctrine()
                 ->getRepository('EntityBundle:Video')
-                ->findBy(array('authorId'=>$user->getId()), array('id' => 'desc'));
-            
-            return $this->render('HamsterHubBundle:Dashboard:index.html.twig', array('user'=>$user, 'url'=>$url));
+                ->findBy(array('authorId' => $user->getId()), array('id' => 'desc'));
+
+            return $this->render('HamsterHubBundle:Dashboard:index.html.twig', array('user' => $user, 'url' => $url));
         } else {
-            header('Location: /channel/' . $username);
+            header('Location: /');
             exit;
         }
+    }
+
+    public function editAction($id)
+    {
+        if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            
+        }
+
+    }
+
+    public function removeAction($id)
+    {
+        if ($this->container->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            $video = $this->getDoctrine()
+                ->getRepository('EntityBundle:Video')
+                ->findOneById(array($id));
+
+            if ($video->getAuthorId() === $this->container->get('security.context')->getToken()->getUser()->getId()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->remove($video);
+                $em->flush();
+            }
+
+            return $this->redirectToRoute('hamster_hub_dashboard');
+        }
+
+        return $this->redirectToRoute('hamster_hub_homepage');
     }
 }
